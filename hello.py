809215f106dd2e44bf9ee87ebe2dd367ad1b1f09
Burak Bekircan <burakbekircan61@gmail.com>
2025-12-29 23:07:21 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,11 @@
 name = input("Adınız nedir? ")
-age = input("Kaç yaşındasınız? ")
+age = int(input("Kaç yaşındasınız? "))
 
 print("Merhaba", name)
-print("Yaşınız:", age)
+
+if age >= 18:
+    print(name, "reşitsiniz.")
+else:
+    print(name, "reşit değilsiniz.")
+
 print(name, "GitHub'daki ilk Python programınızı çalıştırıyorsunuz!")
